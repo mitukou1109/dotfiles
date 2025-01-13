d0af343265ecaa3e870c35e98bd7a130e8c03013
mitukou1109 <mitukou1109@gmail.com>
2025-01-13 18:58:19 +0900

    avoid installing setup script

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 for path in "$(cd "$(dirname "$0")"; pwd)/"*; do
   name=$(basename "$path")
-  if [ "$name" != "install.sh" ]; then
+  if [ "$name" != "install.sh" ] && [ "$name" != "setup_zsh.sh" ]; then
     ln -snfv "$path" "$HOME/.$name"
   fi
 done
