cf40e96a6c39eac165e3dce8d48ee027ec72837e
mitukou1109 <mitukou1109@gmail.com>
2026-08-23 18:14:16 +0900

    update install script

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 for path in "$(cd "$(dirname "$0")"; pwd)/"*; do
   name=$(basename "$path")
-  if [ "$name" != "install.sh" ] && [ "$name" != "setup_zsh.sh" ]; then
+  if [ "$name" != "install.sh" ] && [ ! -d "$path" ]; then
     ln -snfv "$path" "$HOME/.$name"
   fi
 done
