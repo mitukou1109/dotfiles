5fd45a7e1f878349d486db40e8b25e3ea548f2a3
mitukou1109 <mitukou1109@gmail.com>
2026-08-23 18:47:22 +0900

    update install script

--- install.sh
+++ install.sh
@@ -2,7 +2,16 @@
 
 for path in "$(cd "$(dirname "$0")"; pwd)/"*; do
   name=$(basename "$path")
-  if [ "$name" != "install.sh" ] && [ ! -d "$path" ]; then
+  
+  if [ "$name" = "install.sh" ] || [ "$name" = "setup_ros.sh" ]; then
+    continue
+  fi
+
+  if [ "$name" = "starship.toml" ]; then
+    mkdir -p "$HOME/.config"
+    ln -snfv "$path" "$HOME/.config/starship.toml"
+  else
     ln -snfv "$path" "$HOME/.$name"
   fi
 done
+
